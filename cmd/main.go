a323d8bffc166d139ab0d3b666173b8878d134c0
Brian Quirion <quirion.brian@gmail.com>
2023-02-23 20:30:52 -0500

    feat(core): Add gofiber

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"goChessGame/internal/chess_game/adapters/driving"
+	"log"
+
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+var (
+	port = flag.String("port", ":3000", "Port to listen on")
+	prod = flag.Bool("prod", false, "Enable prefork in Production")
+)
 
 func main() {
-	fmt.Print("Hi.")
+	fmt.Print("Starting up...")
+
+	// Parse command-line flags
+	flag.Parse()
+
+	// Create fiber app
+	app := fiber.New(fiber.Config{
+		Prefork: *prod, // go run app.go -prod
+	})
+
+	// Middleware
+	app.Use(recover.New())
+	app.Use(logger.New())
+
+	// Create a /api/v1 endpoint
+	v1 := app.Group("/api/v1")
+
+	// Bind handlers
+	v1.Get("/ping", driving.Ping)
+
+	// Setup static files
+	app.Static("/", "./static/public")
+
+	// Handle not founds
+	app.Use(driving.NotFound)
+
+	// Listen on port 3000
+	log.Fatal(app.Listen(*port)) // go run app.go -port=:3000
 }
---
+++ internal/chess_game/adapters/driving/http_core.go
@@ -0,0 +1,13 @@
+package driving
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func Ping(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{})
+}
+
+func NotFound(c *fiber.Ctx) error {
+	return c.Status(404).SendFile("./assets/404.html")
+}
